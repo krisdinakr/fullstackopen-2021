09ed078d2e53588df9ce7aef93d83417f047b34a
krisdina <krssdina@gmail.com>
2021-08-07 12:30:51 +0700

    add number input and handler

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,16 +2,21 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addContact = (event) => {
     event.preventDefault();
 
     const newContactObj = {
+      key: newName,
       name: newName,
-      key: newName
+      number: newNumber
     };
     
     const checkDuplicate = persons.find(contact => contact.name === newName);
@@ -23,17 +28,22 @@ const App = () => {
     }
 
     setNewName('');
+    setNewNumber('');
   }
 
-  const changeHandler = (event) => {
-    setNewName(event.target.value);
-  }
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input />
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={changeHandler} />
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
         </div>
         <div>
           <button type="submit" onClick={addContact}>add</button>
@@ -41,7 +51,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
       </div>
     </div>
   )
