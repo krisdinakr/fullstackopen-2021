745ee855564cc64d13e2bb222ee228540e75fb59
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2021-10-08 17:59:56 +0700

    add morgan

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = 3001;
 
+
 app.use(express.json());
+morgan.token('body', (req) => { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 let persons = [
   { 
