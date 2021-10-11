644dc6f8fe52789ad737bcda7e01474b96fbfaa1
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2021-10-11 16:10:27 +0700

    add cors

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-
+app.use(cors());
 app.use(express.json());
 morgan.token('body', (req) => { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')
