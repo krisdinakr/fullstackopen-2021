a80554096bbacdd6dae7b2d21b1d3e70988da007
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2021-10-19 09:53:17 +0700

    update build

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -13,6 +13,8 @@ morgan.token('body', (req) => { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
 
+console.log('DATABASE', process.env.MONGODB_URI)
+
 app.get('/api/persons', (req, res, next) => {
   Person.find({})
   .then(persons => {
