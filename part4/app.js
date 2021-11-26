50d07ab6d044c500056f2d401995d44d8ca732c9
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2021-11-26 09:02:44 +0700

    add controller and script for e&e testing with cypress

--- part4/app.js
+++ part4/app.js
@@ -8,6 +8,7 @@ const loginRouter = require('./controllers/login');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const middleware = require('./utils/middleware');
+const testingRouter = require('./controllers/testing');
 
 const app = express();
 
@@ -22,9 +23,12 @@ app.use(express.json());
 app.use(morgan('tiny'));
 
 app.use('/api/login', loginRouter);
-
 app.use('/api/blogs', middleware.tokenExtractor, middleware.userExtractor, blogRouter);
 app.use('/api/users', userRouter);
+
+if (process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter);
+}
 
 app.use(middleware.errorHandler);
 
